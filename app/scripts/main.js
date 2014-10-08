7704096a000ba81ce6efbffc8540e752f3969110
Laura Patti <lpatti10@gmail.com>
2014-10-08 11:36:52 -0400

    Partial template complete

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,41 @@
-console.log('The Iron Yard Rocks');
+var currentSlide = 0;
+
+$(function() {
+	$(document).keyup(function(e) {
+		// to get keycodes use demo field on this page: http://api.jquery.com/keyup
+		if(e.keyCode == 39 || e.keyCode == 40 || e.keyCode == 32) next();
+		else if(e.keyCode == 37 || e.keyCode == 38) back();
+		else if(e.keyCode == 87) $('.slides').fadeToggle();
+	});
+	$('.slides').swipe({
+		swipe:function(event, direction, distance, duration, fingerCount) {
+			switch(direction) {
+				case "left":
+					next();
+					break;
+				case "right":
+					back();
+					break;
+			}
+		}
+	});
+	initSlides();
+});
+
+function initSlides() {
+	$('section').eq(currentSlide).addClass('active');
+}
+
+function next() {
+	goto(currentSlide+1);
+}
+
+function back() {
+	goto(currentSlide-1);
+}
+
+function goto(n) {
+	if(n > -1 && n < $('section').length) currentSlide = n;
+	else return;
+	$('section').removeClass('active').eq(currentSlide).addClass('active');
+}
